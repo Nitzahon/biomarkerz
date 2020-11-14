9b03c4ebf75cf5aa2a80ffe851257783cabf43f4
Nitzahon <nadavga@gmail.com>
2020-11-14 21:12:48 +0200

    added visualizer only working option is react-mic

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App row justify-content-md-center">
       <Frame />
+
     </div>
   );
 }
--- src/components/Frame.js
+++ src/components/Frame.js
@@ -1,32 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {ReactMic} from "react-mic";
 import Webcam from "react-webcam";
-import SurveyComp from "./SurveyComp"
+import SurveyComp from "./SurveyComp";
 export default class Frame extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-
+        this.state = {
+            record:false
         }
     }
+    componentDidMount() {
+        var video = document.querySelector('video');
+        video.addEventListener('loadedmetadata', event => {
+            console.log(video.videoWidth, video.videoHeight)
 
+        });
+        this.setState({record:true});
+
+     }
     render() {
 
         const videoConstraints = {
-            width: {max:1920},
-            height: {max:1080},
+            width: { max: 1280 },
+            height: { max: 720 },
             facingMode: "user"
-          };
+        };
         return (
             <div className="frame row justify-content-md-center">
-                       <div className="vid">
-                           <Webcam
+                <div className="vid">
+                    <Webcam id="videmo"
+                        audio={false}
                         videoConstraints={videoConstraints}
-                        width={960}
-                        height={540}
+                        width={640}
+                        height={360}
                     />
-                       </div>
+                    <ReactMic
+                        record={this.state.record}
+                        className="sound-wave"
+                        onStop={this.onStop}
+                        onData={this.onData}
+                        strokeColor="#FFFFFF"
+                        backgroundColor="#000000" />
+                </div>
                 <div className="surv">
-                    <SurveyComp/>
+                    <SurveyComp />
                 </div>
             </div>
         )
